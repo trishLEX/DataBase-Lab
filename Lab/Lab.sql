d174043121eb4ba84b137e4b9db40b3a1a25b193
Alexey <alexeytr5@gmail.com>
2017-11-26 16:27:43 +0300

    Триггеры переписаны с минимальным использованием курсоров.

--- Lab/Lab.sql
+++ Lab/Lab.sql
@@ -72,7 +72,7 @@ CREATE TABLE ShopDB.ShopSchema.[Shopman]
 CREATE TABLE ShopDB.ShopSchema.[Check]
 (
     checkID INT PRIMARY KEY NOT NULL IDENTITY(0, 1),
-    date DATE NOT NULL,
+    date DATETIME NOT NULL,
     totalCost MONEY NOT NULL,
     typeOfPay BIT NOT NULL,
     discount SMALLINT DEFAULT 0,
@@ -81,12 +81,22 @@ CREATE TABLE ShopDB.ShopSchema.[Check]
 )
   GO
 
-CREATE TABLE ShopDB.ShopSchema.[Card]
+CREATE TABLE ShopDB.ShopSchema.[Card_Subtype]
 (
   checkID INT PRIMARY KEY NOT NULL,
   FOREIGN KEY (checkID) REFERENCES ShopDB.ShopSchema.[Check] (checkID),
 
-  cardID INT NOT NULL UNIQUE IDENTITY(0, 1),
+  cardID INT NOT NULL,
+  FOREIGN KEY (cardID) REFERENCES ShopDB.ShopSchema.[Card] (cardID)
+)
+  GO
+
+CREATE TABLE ShopDB.ShopSchema.[Card]
+(
+  checkID INT NOT NULL,
+  FOREIGN KEY (checkID) REFERENCES ShopDB.ShopSchema.[Check] (checkID),
+
+  cardID INT PRIMARY KEY NOT NULL IDENTITY(0, 1),
   type BIT DEFAULT 0,
   phone CHAR(11) NOT NULL,
   firstName VARCHAR(25) NOT NULL,
@@ -96,15 +106,25 @@ CREATE TABLE ShopDB.ShopSchema.[Card]
 
 CREATE TABLE ShopDB.ShopSchema.[Event]
 (
-  checkID INT PRIMARY KEY NOT NULL,
+  checkID INT,
   FOREIGN KEY (checkID) REFERENCES ShopDB.ShopSchema.[Check] (checkID),
 
-  eventID INT NOT NULL UNIQUE IDENTITY(0, 1),
+  eventID INT PRIMARY KEY NOT NULL IDENTITY(0, 1),
   description VARCHAR(500) NOT NULL,
   expDate DATE NOT NULL,
 )
   GO
 
+CREATE TABLE ShopDB.ShopSchema.[Event_Subtype]
+(
+  checkID INT PRIMARY KEY NOT NULL,
+  FOREIGN KEY (checkID) REFERENCES ShopDB.ShopSchema.[Check] (checkID),
+
+  eventID INT NOT NULL,
+  FOREIGN KEY (eventID) REFERENCES ShopDB.ShopSchema.[Event] (eventID)
+)
+
+  GO
 CREATE TABLE ShopDB.ShopSchema.[Item]
 (
   itemID INT PRIMARY KEY NOT NULL IDENTITY(0, 1),
@@ -117,7 +137,8 @@ CREATE TABLE ShopDB.ShopSchema.[Item]
 CREATE TABLE ShopDB.ShopSchema.[Check_Item_INT]
 (
   checkID INT NOT NULL,
-  itemID INT NOT NULL,
+  itemID  INT NOT NULL,
+  count   INT NOT NULL DEFAULT 1,
 
   PRIMARY KEY (checkID, itemID),
 
@@ -351,6 +372,23 @@ AS
   OPEN @checks_cursor
 GO
 
+CREATE FUNCTION ShopSchema.search_shopCode_by_shopmanCode(@shopmanCode INT) RETURNS INT WITH SCHEMABINDING
+AS
+BEGIN
+  DECLARE @shopCode INT
+  SELECT @shopCode = shopCode FROM ShopSchema.Shopman WHERE @shopmanCode = shopmanCode
+  RETURN @shopCode
+END
+GO
+
+CREATE VIEW ShopSchema.[Items_in_check] WITH SCHEMABINDING
+AS
+SELECT C.checkID, C.date, C.totalCost, C.typeOfPay, C.discount, C.shopmanCode, I.itemID, I.itemName, I.description, I.country, INT.count, S.price
+FROM ShopSchema.[Check] AS C
+  JOIN ShopSchema.Check_Item_INT AS INT ON C.checkID = INT.checkID
+  JOIN ShopSchema.Item AS I ON INT.itemID = I.itemID
+  JOIN ShopSchema.Store AS S ON I.itemID = S.itemID AND ShopSchema.search_shopCode_by_shopmanCode(C.shopmanCode) = S.shopCode
+GO
 
 CREATE TRIGGER ShopSchema.shopman_insert
 ON ShopSchema.Shopman
@@ -539,3 +577,175 @@ AS
 GO
 
 DELETE FROM ShopSchema.Admins WHERE firstName = 'asd'
+
+CREATE VIEW ShopSchema.[Checks_with_cards_and_items] WITH SCHEMABINDING
+AS
+SELECT C.checkID, C.date, C.totalCost, C.typeOfPay, C.discount, C.shopmanCode,
+  IT.itemID, IT.itemName, IT.description, IT.country, IT.count, IT.price,
+  CD.cardID, CD.type, CD.phone, CD.lastName, CD.firstName FROM ShopSchema.[Check] AS C
+JOIN ShopSchema.Card_Subtype AS S ON C.checkID = S.checkID
+JOIN ShopSchema.Card AS CD ON CD.cardID = S.cardID
+JOIN ShopSchema.Items_in_check AS IT ON IT.checkID = C.checkID
+GO
+
+CREATE TRIGGER ShopSchema.card_insert
+ON ShopSchema.Card
+INSTEAD OF INSERT
+AS
+  IF (EXISTS(SELECT * FROM inserted WHERE phone NOT LIKE '8%'))
+      BEGIN
+        SELECT * FROM inserted WHERE phone NOT LIKE '8%';
+        THROW 50000, 'Invalid phone', 1
+      END
+  ELSE
+    INSERT INTO ShopSchema.Card (type, phone, firstName, lastName) SELECT inserted.type, inserted.phone, inserted.firstName, inserted.lastName FROM inserted
+GO
+
+INSERT INTO ShopSchema.Card (type, phone, firstName, lastName) VALUES (0, '99854486356', 'a', 'b')
+
+CREATE TRIGGER ShopSchema.card_delete
+ON ShopSchema.Card
+INSTEAD OF DELETE
+AS
+  IF (NOT EXISTS(SELECT * FROM deleted
+    JOIN ShopSchema.Card_Subtype ON deleted.cardID = Card_Subtype.cardID
+    JOIN ShopSchema.[Check] ON Card_Subtype.checkID = [Check].checkID
+    WHERE ShopSchema.calculateAge([Check].date) > 1))
+      THROW 50000, 'There are checks, that are dated less than year ago', -1
+  ELSE
+    DELETE FROM Card WHERE cardID IN (SELECT deleted.cardID FROM deleted)
+GO
+
+CREATE TRIGGER ShopSchema.card_update
+ON ShopSchema.Card
+AFTER UPDATE
+AS
+  IF UPDATE(type) OR UPDATE(cardID)
+      BEGIN
+        RAISERROR('Columns type or cardID are tried to update', 10, 1)
+        ROLLBACK
+      END
+GO
+
+UPDATE ShopSchema.Card SET type = 0 WHERE cardID = 0
+GO
+
+CREATE TRIGGER ShopSchema.tr_insert
+ON ShopSchema.Checks_with_cards_and_items
+INSTEAD OF INSERT
+AS
+  BEGIN
+    DECLARE @checkID INT
+    DECLARE @date DATETIME
+    DECLARE @totalCost MONEY
+    DECLARE @typeOfPay BIT
+    DECLARE @discount SMALLINT
+    DECLARE @shopmanCode INT
+    DECLARE @itemID INT
+    DECLARE @itemName VARCHAR(100)
+    DECLARE @description VARCHAR(100)
+    DECLARE @country VARCHAR(58)
+    DECLARE @count INT
+    DECLARE @price MONEY
+    DECLARE @cardID INT
+    DECLARE @type BIT
+    DECLARE @phone CHAR(11)
+    DECLARE @lastName VARCHAR(25)
+    DECLARE @firstName VARCHAR(25)
+
+    DECLARE @cursor CURSOR
+    DECLARE @error_msg VARCHAR(50)
+
+    SET @cursor = CURSOR FORWARD_ONLY STATIC
+      FOR SELECT  inserted.checkID, inserted.date, inserted.totalCost, inserted.typeOfPay, inserted.discount, inserted.shopmanCode, inserted.itemID, inserted.itemName, inserted.description,
+                  inserted.country, inserted.count, inserted.price, inserted.cardID, inserted.type, inserted.phone, inserted.lastName, inserted.firstName FROM inserted
+    OPEN @cursor
+
+    FETCH NEXT FROM @cursor INTO @checkID, @date, @totalCost, @typeOfPay, @discount, @shopmanCode, @itemID, @itemName, @description, @country, @count, @price, @cardID, @type, @phone, @lastName, @firstName
+
+    WHILE (@@FETCH_STATUS = 0)
+    BEGIN
+      IF @itemID NOT IN (SELECT I.itemID FROM ShopSchema.Item AS I)
+        BEGIN
+          SET @error_msg = 'It''s forbidden to paste new items in this view. Wrong itemID: ' + CAST(@itemID AS VARCHAR)
+          RAISERROR(@error_msg, 10, 1)
+        END
+      ELSE IF(@itemName NOT IN (SELECT I.itemName FROM ShopSchema.Item AS I))
+        BEGIN
+          SET @error_msg = 'Item with name: ' + @itemName + ' doesn''t exist'
+          RAISERROR(@error_msg, 10, 1)
+        END
+      ELSE IF (NOT EXISTS(SELECT I.itemID FROM ShopSchema.Item AS I WHERE I.itemName = @itemName AND I.itemID = @itemID))
+        BEGIN
+          SET @error_msg = 'ItemID: ' + CAST(@itemID AS VARCHAR) + ' doesn''t correspond with ItemName: ' + @itemName
+          RAISERROR(@error_msg, 10, 1)
+        END
+      IF (@checkID IS NULL)
+        BEGIN
+          IF ((SELECT SUM(inserted.count * inserted.price * (100 - @discount) / 100) FROM inserted WHERE inserted.date = @date) != @totalCost)
+            THROW 50000, 'TotalCost in check doesn''t equal to sum of items price', 1
+
+          IF (@date IN (SELECT date FROM ShopSchema.[Check]))
+            SELECT @checkID = checkID FROM ShopSchema.[Check] WHERE [Check].date = @date AND [Check].totalCost = @totalCost
+          ELSE
+            BEGIN
+              INSERT INTO ShopSchema.[Check] (date, totalCost, typeOfPay, shopmanCode)
+                VALUES (@date, @totalCost, @typeOfPay, @shopmanCode)
+              SET @checkID = ident_current('ShopSchema.[Check]')
+            END
+
+          INSERT INTO ShopSchema.Check_Item_INT (checkID, itemID) VALUES (@checkID, @itemID)
+
+          IF (@discount != 0)
+            BEGIN
+              IF (@cardID NOT IN (SELECT cardID FROM ShopSchema.Card))
+                BEGIN
+                  INSERT INTO ShopSchema.Card (type, phone, firstName, lastName)
+                    VALUES (@type, @phone, @firstName, @lastName)
+                  SET @cardID = ident_current('ShopSchema.Card')
+                END
+              INSERT INTO ShopSchema.Card_Subtype (checkID, cardID) VALUES (@checkID, @cardID)
+            END
+        END
+      ELSE
+        BEGIN
+          IF ((SELECT SUM(inserted.count * inserted.price) FROM inserted WHERE @checkID = inserted.checkID) != @totalCost)
+            THROW 50000, 'TotalCost in check doesn''t equal to sum of items price', 1
+
+          INSERT INTO ShopSchema.Check_Item_INT (checkID, itemID) VALUES (@checkID, @itemID)
+        END
+      FETCH NEXT FROM @cursor INTO @checkID, @date, @totalCost, @typeOfPay, @discount, @shopmanCode, @itemID, @itemName, @description, @country, @count, @price, @cardID, @type, @phone, @lastName, @firstName
+    END
+    CLOSE @cursor
+    DEALLOCATE @cursor
+  END
+GO
+
+INSERT INTO ShopSchema.Checks_with_cards_and_items (date, totalCost, typeOfPay, shopmanCode, itemID, itemName, description, country, price)
+    VALUES ('2017-11-26T15:47:00', 6900, 0, 0, 10, '504 Regular Straight Jeans', 'Live in Levi''s', 'Turkey', 6900)
+
+INSERT INTO ShopSchema.Checks_with_cards_and_items (date, totalCost, typeOfPay, discount, shopmanCode, itemID, itemName, description, country, count, price, cardID, type, phone, lastName, firstName)
+    VALUES ('2015-11-26T15:48:00', 6210, 0, 10, 0, 10, '504 Regular Straight Jeans', 'Live in Levi''s', 'Turkey', 1, 6900, 0, 1, '89888432908', 'Копылов', 'Ленур')
+
+CREATE TRIGGER ShopSchema.tr_delete
+ON ShopSchema.Checks_with_cards_and_items
+INSTEAD OF DELETE
+AS
+BEGIN
+  IF ((SELECT MIN(ShopSchema.calculateAge(deleted.date)) FROM deleted) < 1)
+    THROW 50000, 'There are checks, that are dated less than year ago', 1
+  DELETE FROM ShopSchema.Check_Item_INT WHERE Check_Item_INT.checkID IN (SELECT deleted.checkID FROM deleted)
+  DELETE FROM ShopSchema.Card_Subtype WHERE Card_Subtype.checkID IN (SELECT deleted.checkID FROM deleted)
+  DELETE FROM ShopSchema.[Check] WHERE [Check].checkID IN (SELECT deleted.checkID FROM deleted)
+END
+
+DELETE FROM ShopSchema.Checks_with_cards_and_items WHERE checkID = 13504
+
+CREATE TRIGGER ShopSchema.tr_update
+ON ShopSchema.Checks_with_cards_and_items
+INSTEAD OF UPDATE
+AS
+  BEGIN
+    RAISERROR('It''s not allowed to update columns from this view', 10, 1)
+  END
+
